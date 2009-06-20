b4a022bb90eac56b3653f0e89cc55bdb6c3f2f0a
Michael Christenson II <m3talsmith@gmail.com>
2009-06-19 23:53:34 -0400

    added spork as a gem dependency

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  
+  config.gem "spork"
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
